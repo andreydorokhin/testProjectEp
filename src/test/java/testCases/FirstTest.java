2f57cbf98f4129c6fd3664a45c3cef8e95690e7d
Andrey_Dorokhin <andrey.dorokhin.dam@gmail.com>
2019-02-12 19:08:48 +0200

    first test with checking result of searching

--- src/test/java/testCases/FirstTest.java
+++ src/test/java/testCases/FirstTest.java
@@ -2,6 +2,8 @@ package testCases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,11 +14,12 @@ import ua.bytes.config.Driver;
 
 public class FirstTest {
     private static WebDriver webDriver;
-//    private static Driver driver;
 
     @BeforeClass
     public void init() {
-//        driver = Driver.getDriver();
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
+
         webDriver = Driver.getDriver().getWebDriver();
 
         webDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -26,14 +29,43 @@ public class FirstTest {
 
     @Test
     public void EnterText() throws InterruptedException {
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
+
         webDriver.findElement(By.name("q")).sendKeys("automation");
         webDriver.findElement(By.name("q")).submit();
-        System.out.println("Facebook Entered in Textbox");
-        Thread.sleep(5000);
+        System.out.println("word automation Entered in Textbox");
+//        Thread.sleep(5000);
+//        System.out.println("Page title is: " + webDriver.getTitle());
+//
+//        (new WebDriverWait(webDriver, 10)).until(new ExpectedCondition <Boolean>() {
+//            public Boolean apply(WebDriver d) {
+//                return d.getTitle().toLowerCase().startsWith("automation");
+//            }
+//        });
+//
+//        System.out.println("Page title is: " + webDriver.getTitle());
+    }
+
+    @Test
+    public void checkResultOfSearching() throws InterruptedException {
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
+
+        (new WebDriverWait(webDriver, 10)).until(new ExpectedCondition <Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("automation");
+            }
+        });
+
+        System.out.println("Page title is: " + webDriver.getTitle());
     }
 
     @AfterClass
     public void closebrowser() {
+        long id = Thread.currentThread().getId();
+        System.out.println("Before test-method. Thread id is: " + id);
+
         Driver.closeBrowser();
     }
 }
